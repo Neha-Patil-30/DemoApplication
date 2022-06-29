a84fe6c96c91ec22b7fae8a025dd7b93ad2191aa
Neha-Patil-30 <37060640+Neha-Patil-30@users.noreply.github.com>
2022-06-29 16:31:08 +0530

    Add files via upload

---
+++ tweetapp/src/main/java/com/tweetapp/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.tweetapp.repository;
+
+import java.util.List;
+
+import com.tweetapp.model.Users;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<Users, String> {
+
+    public Users findByLoginId(String loginId);
+
+    public Users findByEmail(String email);
+
+    
+
+}
